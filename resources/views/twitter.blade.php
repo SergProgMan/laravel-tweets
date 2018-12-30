5019c27addb2eb468edcd137a54c5c75adb1d39e
SergProgMan <sembratyev@gmail.com>
2018-12-30 17:45:35 +0200

    created form  for view tweets, lkes and retweets

--- resources/views/twitter.blade.php
+++ resources/views/twitter.blade.php
@@ -10,5 +10,34 @@
     </div>
 </nav>
 
+<div class="container">
+    <form class="well" action="{{route('post.tweet')}}" method="post" enctype="multipart/form-data">
+        {{csrf_field()}}
+        @if(count($errors) > 0)
+            @foreach ($errors->all() as $error)
+
+            @endforeach
+        @endif
+    </form>
+
+    @if(!empty($data))
+        @foreach($data as $key=>$tweet)
+            <div class="well">
+                <h3>{{ $tweet['text'] }}
+                    <i class="glyphicon glyphicon-heart"></i>{{ $tweet['favorite_count']}}
+                    <i class="glyphicon glyphicon-repeat"></i>{{ $tweet['retweet_count']}}
+                </h3>
+
+                @if(!empty($tweet['extended_entities']['media']))
+                    @foreach($tweet['extended_entities']['media'] as $i)
+                        <img src="{{ $i['media_url_https'] }}" style="width:100px">                    
+                    @endforeach
+                @endif
+            </div>
+        @endforeach
+    @else
+        <p>No Tweets Found...</p>
+    @endif
+</div>
 
 @endsection
