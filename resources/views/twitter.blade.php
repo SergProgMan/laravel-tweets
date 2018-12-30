3f431a0cebccac0a926be26466a509517e067324
SergProgMan <sembratyev@gmail.com>
2018-12-30 18:34:15 +0200

    added form for post images

--- resources/views/twitter.blade.php
+++ resources/views/twitter.blade.php
@@ -15,9 +15,24 @@
         {{csrf_field()}}
         @if(count($errors) > 0)
             @foreach ($errors->all() as $error)
-
+                <div class="alert alert-danger">
+                    {{ $error }}
+                </div>
             @endforeach
         @endif
+
+        <div class="form-group">
+            <label>Tweet Text</label>
+            <input type="text" name="tweet" class="form-control">
+        </div>
+        <div class="form-group">
+            <label>Upload Images</label>
+            <input type="file" name="images[]" multiple class="form-control">
+        </div>
+        <div class="form-group">
+            <button class="btn btn-success">Create Tweet </button>
+        </div>
+
     </form>
 
     @if(!empty($data))
